982b1413a7fb2961d7105ce9ac7ec7b013fe18ea
nik <niklas.harnish@hotmail.com>
2024-01-26 23:08:39 +0100

    added some slicing mechanics

--- Choose the Ooze/Assets/Scripts/Ingredient.cs
+++ Choose the Ooze/Assets/Scripts/Ingredient.cs
@@ -15,6 +15,17 @@ public class Ingredient : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetButtonDown("Fire1")) {
+            Vector3 mousePos = Input.mousePosition;
+            Slice(mousePos.y);
+        }
     }
+
+    public void Slice(float slicePointX) {
+        SpriteMask mask = this.GetComponentInChildren<SpriteMask>();
+        Transform maskTransform = mask.gameObject.transform;
+        maskTransform.position = new Vector3(slicePointX, maskTransform.transform.position.y, 0);
+
+        mask.gameObject.SetActive(true); 
+   }
 }
