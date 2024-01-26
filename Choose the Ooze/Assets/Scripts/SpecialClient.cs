ed3ef4dd6fee2b5fce7d7f403d470065eecd7db2
decimatedTomato <greentomatopower@gmail.com>
2024-01-27 00:04:40 +0100

    Intermediate state of ClientRequest related scripts

---
+++ Choose the Ooze/Assets/Scripts/SpecialClient.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SpecialClientType
+{
+    NONE,
+    FISH,
+    WIZARD,
+    VIKING,
+    CULTIST,
+    SCIENTIST,
+    PRINCESS,
+    MILKMAN,
+    CHILD,
+}
