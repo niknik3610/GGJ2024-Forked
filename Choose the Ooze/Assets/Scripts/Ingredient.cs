1b8da314698f1c2b6b0ce5fd6bcd3c5092fe9b48
nik <niklas.harnish@hotmail.com>
2024-01-26 23:23:16 +0100

    removed unused files

--- Choose the Ooze/Assets/Scripts/Ingredient.cs
+++ Choose the Ooze/Assets/Scripts/Ingredient.cs
@@ -1,31 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Ingredient : MonoBehaviour
 {
     public string ingredinetName {get; set;}
     public bool beingCarried {get; set;} = false;
+    private Camera m_Camera;
+
+    public SpriteMask hidingMask;
+
+    void Awake()
+    {
+        m_Camera = Camera.main;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        hidingMask = this.GetComponentInChildren<SpriteMask>();
+        hidingMask.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Fire1")) {
-            Vector3 mousePos = Input.mousePosition;
-            Slice(mousePos.y);
+        Mouse mouse = Mouse.current;
+        if (mouse.leftButton.wasPressedThisFrame)
+        {
+            Vector3 mousePosition = mouse.position.ReadValue();
+            Ray ray = m_Camera.ScreenPointToRay(mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                if(hit.collider.gameObject == this.gameObject)
+                {
+                    Slice(Camera.main.ScreenToWorldPoint(mousePosition).y);
+                }
+            }
         }
     }
 
     public void Slice(float slicePointX) {
-        SpriteMask mask = this.GetComponentInChildren<SpriteMask>();
-        Transform maskTransform = mask.gameObject.transform;
+        Transform maskTransform = hidingMask.gameObject.transform;
         maskTransform.position = new Vector3(slicePointX, maskTransform.transform.position.y, 0);
 
-        mask.gameObject.SetActive(true); 
+        hidingMask.gameObject.SetActive(true); 
    }
 }
