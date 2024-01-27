a5115ede13d92ee25dd5d3fa7acf3e5df3fc9bb4
decimatedTomato <greentomatopower@gmail.com>
2024-01-27 03:01:39 +0100

    whatever

--- Choose the Ooze/Assets/Scripts/Client.cs
+++ Choose the Ooze/Assets/Scripts/Client.cs
@@ -7,6 +7,10 @@ public class ClientRequestGenerator
 {
     Queue<ClientRequest> generateRequests(SpecialClientType clientType)
     {
+<<<<<<< HEAD
+=======
+        // TODO add random number of requests here
+>>>>>>> 0c04f2d (whatever)
         return new Queue<ClientRequest>();
     }
 }
