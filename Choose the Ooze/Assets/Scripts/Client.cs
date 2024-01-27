0bd5db105086662e89e49a38ef678a3d33644c72
nik <niklas.harnish@hotmail.com>
2024-01-27 10:56:55 +0100

    fixed client.cs

--- Choose the Ooze/Assets/Scripts/Client.cs
+++ Choose the Ooze/Assets/Scripts/Client.cs
@@ -7,10 +7,6 @@ public class ClientRequestGenerator
 {
     Queue<ClientRequest> generateRequests(SpecialClientType clientType)
     {
-<<<<<<< HEAD
-=======
-        // TODO add random number of requests here
->>>>>>> 0c04f2d (whatever)
         return new Queue<ClientRequest>();
     }
 }
